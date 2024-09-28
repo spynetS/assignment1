8e098860dc5135d6b012a8d7d5911b645d9524bd
Rahaf021 <jora23iv@student.ju.se>
2024-09-28 03:16:04 +0200

    Add files via upload

---
+++ Model/InvalidTokenException.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Calculator.Model
+{
+    ///<summary>
+    ///This class returns an exception when the operand is not a number 
+    ///</summary>
+
+
+    public class InvalidTokenException : Exception
+    {
+        public InvalidTokenException()
+        {
+
+        }
+        public InvalidTokenException(string message)
+        : base(message)
+        {
+
+        }
+
+    }
+
+}
